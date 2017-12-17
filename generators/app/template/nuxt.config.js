5f1937eca6b53060904a2797914372f575adcb00
WilliamDASILVA <william.da.silva@outlook.com>
2017-12-17 13:08:03 +0100

    Init yeoman generator, still in WIP

---
+++ generators/app/template/nuxt.config.js
@@ -0,0 +1,50 @@
+// const postcssNested = require('postcss-nested');
+// const postcssResponsiveType = require('postcss-responsive-type');
+// const postcssHexrgba = require('postcss-hexrgba');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/css/main.css'],
+  postcss: [
+    autoprefixer(),
+    // postcssNested(),
+    // postcssResponsiveType(),
+    // postcssHexrgba(),
+  ],
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: ['axios'],
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
